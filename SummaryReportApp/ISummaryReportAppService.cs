fb34c5f801b534753d906f772e8b3c8b9dc66224
nrshaaam <122845620+nrshaaam@users.noreply.github.com>
2023-07-17 09:22:33 +0800

    Add files via upload

---
+++ SummaryReportApp/ISummaryReportAppService.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services;
+using SOTSCanvas.HubOutboundModels;
+using SOTSCanvas.InboundfModel;
+using SOTSCanvas.SummaryReportModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTSCanvas.SummaryReportApp
+{
+    public interface ISummaryReportAppService : IApplicationService
+    {
+
+
+        reportDetail mainSearch(string reportType, DateTime dateStart, DateTime dateEnd );
+
+    }
+}
---
+++ SummaryReportApp/SummaryReportAppService.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Application.Services;
+using AutoMapper;
+using SOTSCanvas.HubOutboundModels;
+using SOTSCanvas.InboundfModel;
+using SOTSCanvas.SummaryReportModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTSCanvas.SummaryReportApp
+{
+    public class SummaryReportAppService : ApplicationService, ISummaryReportAppService
+    {
+        private readonly ISummaryReportManager _SummaryReportManager;
+
+        public SummaryReportAppService(ISummaryReportManager SummaryReportManager)
+        {
+            _SummaryReportManager = SummaryReportManager;
+        }
+
+
+        public reportDetail mainSearch(string reportType, DateTime dateStart, DateTime dateEnd)
+        {
+            return _SummaryReportManager.searchMain(reportType, dateStart, dateEnd);
+        }
+    }
+}
