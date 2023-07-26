a190db7b306faa5dd8d84ecc9530d81528f73750
nrshaaam <122845620+nrshaaam@users.noreply.github.com>
2023-07-26 17:49:56 +0800

    Add files via upload

---
+++ DayEndReportModels/IDayEndReportManager.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTSCanvas.DayEndReportModels
+{
+    public interface IDayEndReportManager : IDomainService
+    {
+
+        reportDetail searchMain(string searchReport);
+
+
+        reportDetail getReport(string searchReport);
+
+    }
+
+    
+}
