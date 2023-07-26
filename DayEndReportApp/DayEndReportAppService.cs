77e1349b1ea2eb9b964e6c19cbf4bf3bcaf626bf
nrshaaam <122845620+nrshaaam@users.noreply.github.com>
2023-07-26 17:49:05 +0800

    Add files via upload

---
+++ DayEndReportApp/DayEndReportAppService.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Application.Services;
+using AutoMapper;
+using SOTSCanvas.HubOutboundModels;
+using SOTSCanvas.InboundfModel;
+using SOTSCanvas.DayEndReportModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTSCanvas.DayEndReportApp
+{
+    public class DayEndReportAppService : ApplicationService, IDayEndReportAppService
+    {
+        private readonly IDayEndReportManager _DayEndReportManager;
+
+        public DayEndReportAppService(IDayEndReportManager DayEndReportManager)
+        {
+            _DayEndReportManager = DayEndReportManager;
+        }
+
+
+        public reportDetail mainSearch(string searchReport)
+        {
+            return _DayEndReportManager.searchMain(searchReport);
+        }
+        public reportDetail reportGet(string searchReport)
+        {
+            return _DayEndReportManager.getReport(searchReport);
+        }
+    }
+}
---
+++ DayEndReportApp/IDayEndReportAppService.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Application.Services;
+using SOTSCanvas.HubOutboundModels;
+using SOTSCanvas.InboundfModel;
+using SOTSCanvas.DayEndReportModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTSCanvas.DayEndReportApp
+{
+    public interface IDayEndReportAppService : IApplicationService
+    {
+
+
+        reportDetail mainSearch(string searchReport);
+
+        reportDetail reportGet(string searchReport);
+
+    }
+}
